81bfb12252680871330159fd5e4fc24efac1884f
Ihor <vitrikushihor0101@gmail.com>
2023-08-14 16:19:42 +0300

    add sidebar styles

--- src/components/LotsSideBar/index.jsx
+++ src/components/LotsSideBar/index.jsx
@@ -1,5 +1,7 @@
-import {AiFillCloseCircle} from "react-icons/ai";
+import {AiFillCloseCircle, AiOutlineLink} from "react-icons/ai";
 import styles from "./styles.module.scss"
+import {BiUser} from "react-icons/bi";
+import {BsBuildings} from "react-icons/bs";
 
 export const LotsSideBar = ({refSidebar, lot, toggleDisplayOff}) => {
 	if (lot.length < 0) {
@@ -7,34 +9,60 @@ export const LotsSideBar = ({refSidebar, lot, toggleDisplayOff}) => {
 	}
 	return (
 
-
 		 <div className={styles.container} ref={refSidebar}>
 			 <div className={styles.close} onClick={toggleDisplayOff}><AiFillCloseCircle/></div>
 
-			 <p>Покупець: {lot.buyer}</p>
-			 <p>Продавець: {lot.seller}</p>
+			 <div className={styles.buyer}>
+				 <div><BiUser/> Покупець:</div>
+				 <p>{lot.buyer}</p>
+			 </div>
+			 <div className={styles.buyer}>
+				 <div><BsBuildings/> Продавець:</div>
+				 <p>{lot.seller}</p>
+			 </div>
+
+
+
 
 			 <p>Статус: {lot.lotStatus}</p>
 			 <p>ДК: {lot.dk}</p>
-			 <p>Ціна: {lot.lotTotalPrice}</p>
-			 <div>
+
+			 <div className={styles.price}>Ціна: <span>{lot.lotTotalPrice} грн</span></div>
+
+			 <div className={styles.lotUsers}>
+				 <h3>Учасники Лоту</h3>
 				 {lot.participantNames && lot.participantNames.map(item => (
-						<p>Імя: {item.name} - edrpou {item.edrpou}</p>
+						<div>
+							<p><span><BiUser/> Імя:</span> {item.name}</p>
+							<p>Edrpou: {item.edrpou}</p>
+						</div>
+
 				 ))}
 			 </div>
-			 <p> Силка на лот : {lot.lotURL}</p>
-			 <p>Силка на pdf : {lot.pdfURL}</p>
-			 <div>
+
+			 <div className={styles.links}>
+				 <h3><AiOutlineLink/> Силки:</h3>
+				 <p> Силка на лот : {lot.lotURL}</p>
+				 <p>Силка на pdf : {lot.pdfURL}</p>
+			 </div>
+
+			 <div className={styles.burying}>
+				 <h3>Продукція: </h3>
+				 <div>
+					 <div>Модель:</div>
+					 <div>Ціна Однієї Штуки:</div>
+					 <div>Кількість:</div>
+					 <div>Ціна за все:</div>
+				 </div>
 				 {lot.lotItems && lot.lotItems.map(lotItem => (
 						<div>
-							<p>Модель: {lotItem.model}</p>
-							<p>Ціна однієї штуки: {lotItem.price} грн</p>
-							<p>Кількість: {lotItem.amount}</p>
-							<p>Ціна за все: {lotItem.totalItemPrice} грн</p>
+							<p>{lotItem.model}</p>
+							<p> {lotItem.price} грн</p>
+							<p> {lotItem.amount}</p>
+							<p>{lotItem.totalItemPrice} грн</p>
 						</div>
 				 ))}
 			 </div>
 		 </div>
 	);
 };
-
